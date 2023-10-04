93ac0b7410b2aa7469db531cc0183dc48db1d317
JuanDazaS <jadaza7832@gmail.com>
2023-10-03 22:16:57 -0400

    Header creado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
+import Header from './components/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       Administrador de eventos de programacion
     </div>
   );
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+class Header extends Component{
+    render(){
+        return (
+            <div>este es el header</div>
+        )
+    }
+}
+export default Header
