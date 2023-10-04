968ff11d635bf93b69aa9d08e5d132236c2dfa71
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-10-04 18:13:44 -0400

    Diseño Footer inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import './App.css';
+//import Footer from './components/Footer/footer';
 import Header from './components/Header/Header';
-
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <header>
         <Header />
+        
       </header>
+      <footer>
+        <Footer />
+      </footer>
       
+
+
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import { Component} from 'react';
+import './Footer.css'
+
+class Footer extends Component {
+    render(){
+        return (
+            <div className='footer-container'>
+                <p>PRIME SOFT SOLUTIONS LTDA</p>
+                
+            </div>
+        )
+    }
+}
+
+export default Footer
