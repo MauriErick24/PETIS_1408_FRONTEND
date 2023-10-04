22915083364a04461c2511f40364b1de641ba69a
Erick M <maumbeltran@gmail.com>
2023-10-04 19:48:22 -0400

    header con logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,17 @@
 import { Component} from 'react';
 import './Header.css'
+import Logo from '../../images/escudo-01.png'
 
 class Header extends Component {
     render(){
         return (
             <div className='header-container'>
-                
+                <div>
+                    
+                </div>
+                <div>
+                    <img src={Logo} alt="escudo UMSS"/>
+                </div>
             </div>
         )
     }
