662b326b8084a055b94c816ec72924ea75ea21fa
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-11-02 11:43:28 -0400

    layout de aside nuevo

--- src/views/VentanaBotones.js
+++ src/views/VentanaBotones.js
@@ -1,9 +1,26 @@
-import Asidek from "../components/Asidek";
+import Layout from "../components/input/Layout";
+import React from 'react';
 
-function VentanaBotones(){
-    return (
-        <Asidek />
-    );
+
+function VentanaBotones() {
+  return (
+    <Layout
+      sidebarContent={
+        <div>
+          {/* Contenido específico del Sidebar para Vista1 */}
+          <h3>Sidebar de Vista 1</h3>
+          <p>Opciones de Vista 1</p>
+        </div>
+      }
+      content={
+        <div>
+          {/* Contenido específico del Content para Vista1 */}
+          <h3>Contenido Principal de Vista</h3>
+          <p>Texto principal de Vista 1</p>
+        </div>
+      }
+    />
+  );
 }
 
 export default VentanaBotones;
