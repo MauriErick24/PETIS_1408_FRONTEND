a03aac09b8c2440ad5aa0fcc77e6220828299384
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-11-03 18:25:02 -0400

    Vista organizadores

--- src/views/organizador.js
+++ src/views/organizador.js
@@ -1,10 +1,31 @@
 import React from "react";
-import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import '../assets/css/Organizador.css';
+import SearchBar from "../components/Searchbar";
+import Itemorg from "../components/Itemorg";
+import Btn from "../components/Btn";
+import Flex from '../components/Flex';
 
-const Organizador = () => {
+function Organizador(){
     return(
         <>
-            Vista organizador
+            <div>
+                <h2 className="tit-lista-org">LISTA DE ORGANIZADORES</h2> 
+                <SearchBar />
+            </div>
+            <div>
+                <Itemorg nombre="JALA SOFT" rep="Mario Caceres" correo= "mcar@gmail.com" telef="4245535"/>
+                <Itemorg nombre="UMSA INFORMATICA" rep="JORGE LOPEZ" correo= "mcar@gmail.com" telef="4245535"/>
+                <Itemorg nombre="JALA SOFT" rep="Mario Caceres" correo= "mcar@gmail.com" telef="4245535"/>
+                <Itemorg nombre="JALA SOFT" rep="Mario Caceres" correo= "mcar@gmail.com" telef="4245535"/>
+                <Itemorg nombre="JALA SOFT" rep="Mario Caceres" correo= "mcar@gmail.com" telef="4245535"/>
+            </div>
+            <Flex display="flex" justify-content="flex-end"  >
+                <Btn type='submit' font-size="12px" >CREAR ORGANIZADOR</Btn>
+            </Flex>
+            
+
         </>
     )
 }
