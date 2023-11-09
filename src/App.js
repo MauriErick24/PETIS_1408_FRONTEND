c83d3c9452492e028f8e9d085fd0427dc75bc321
jorge ledezma zeballos <jledezmaze@yahoo.com>
2023-11-08 21:34:51 -0400

    Organizar nuevo

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ import Visualizacionevento from './views/visualizacion-evento'
 import HomeUser from './views/homeUser'
 import VentanaBotones from './views/VentanaBotones'
 
-import Organizador from './views/organizador'
-import Corganizador from './views/corganizador'
+// import Organizador from './views/organizador';
+import Organizadorn from './views/Organizadorn'
+// import Corganizador from './views/corganizador'; 
 import Patrocinador from './views/patrocinador'
 import Reglas from './views/reglas'
 import Premios from './views/premios'
@@ -31,8 +32,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path='/crear/evento' element={<LayoutCreacion main={<CrearEvento/>}/>}/>
-        <Route path='/crear/evento/organizador' element={<LayoutCreacion main={<Organizador/>}/>}/>
-        <Route path='/crear/evento/organizador/corganizador' element={<LayoutCreacion main={<Corganizador/>}/>}/>
+        <Route path='/crear/evento/organizador' element={<LayoutCreacion main={<Organizadorn/>}/>}/> 
+        {/* <Route path='/crear/evento/organizador/corganizador' element={<LayoutCreacion main={<Corganizador/>}/>}/> */}
         <Route path='/crear/evento/patrocinadores' element={<LayoutCreacion main={<Patrocinador/>}/>}/>
         <Route path='/crear/evento/reglas' element={<LayoutCreacion main={<Reglas/>}/>}/>
         <Route path='/crear/evento/premios' element={<LayoutCreacion main={<Premios/>}/>}/>
---
+++ src/views/Organizadorn.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import '../assets/css/Organizadorn.css';
+
+const library = [
+  { title: "Carrera de Informatica", author: "F. Scott Fitzgerald", year: 1925, publisher: "Charles Scribner's Sons", origin: "EE. UU." },
+  { title: "Carrera de Informatica", author: "F. Scott Fitzgerald", year: 1925, publisher: "Charles Scribner's Sons", origin: "EE. UU." },
+  { title: "MEMI", author: "Harper Lee", year: 1960, publisher: "J. B. Lippincott & Co.", origin: "EE. UU." },
+  { title: "JALA SOFT", author: "George Orwell", year: 1949, publisher: "Secker & Warburg", origin: "Reino Unido" },
+  { title:"UMSA - Informatica",author: "Jorge Ledezma", year:1980, publisher:"Diamond", origin: 'bosnia'},
+  { title:"Univ. Gabriel Rene Moreno",author:"cervantes", year: 1965, publisher: "pinguin", origin: 'Spain'},
+  { title:"Ministerio de Educacion",author:"victor hugo", year: 1976,publisher:'mcrgraw', origin:'France'},
+  { title:"Embajada suiza",author:"nemilville", year: 1985,publisher:'Dolmen', origin:'USA'},
+  { title:"Fundacion Bankia",author:"Cesar Ldzm", year: 1945, publisher:"majos", origin:'Peru'},
+  { title:"Cuadros&CIA", author: "maarisabel", year:1965, publisher:"Trico", origin:'Spain'},
+  { title:"Alcaldia de CB", author:'Nicolas cano', year:1965, publisher:'Friends', origin:'UK'},
+];
+
+function Organizadorn() {
+  const [searchInput, setSearchInput] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
+  const resultsPerPage = 3;
+  const [selectedBooks, setSelectedBooks] = useState([]);
+  const [filteredLibrary, setFilteredLibrary] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSelectBook = (book) => {
+    setSelectedBooks([...selectedBooks, book]);
+  };
+
+  const handleRemoveBook = (book) => {
+    setSelectedBooks(selectedBooks.filter(selectedBook => selectedBook !== book));
+  };
+
+  const handleGoToPage = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    const query = searchInput.toLowerCase();
+    const filteredBooks = library.filter(book => book.title.toLowerCase().startsWith(query));
+    setFilteredLibrary(filteredBooks);
+    const totalResults = filteredBooks.length;
+    setTotalPages(Math.ceil(totalResults / resultsPerPage));
+  }, [searchInput, currentPage]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchInput}
+        onChange={handleSearchInput}
+        placeholder="Buscar libros por título..."
+      />
+      <div id="results">
+        {filteredLibrary
+          .slice((currentPage - 1) * resultsPerPage, currentPage * resultsPerPage)
+          .map((book) => (
+            <div key={book.title}>
+              <p>{book.title}</p>
+              <button onClick={() => handleSelectBook(book)}>Seleccionar</button>
+            </div>
+          ))}
+      </div>
+      <div id="pagination">
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button key={index + 1} onClick={() => handleGoToPage(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
+      <h2>Libros Seleccionados</h2>
+      <ul id="selectedBooks">
+        {selectedBooks.map((book) => (
+          <li key={book.title}>
+            {book.title} (Autor: {book.author}, Año: {book.year})
+            <button onClick={() => handleRemoveBook(book)} className="delete-button">X</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Organizadorn;
